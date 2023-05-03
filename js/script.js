8217209559c0a84cc659ec2ad183e01c3d886404
Devilxiosjussara <126517312+Devilxiosjussara@users.noreply.github.com>
2023-05-03 11:55:29 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function abriMenu() {
+    const divMenu = document.getElementById('respMenu').classList;
+    divMenu.toggle('abrir')
+    const ic = document.getElementById('icone')
+    if(divMenu.contains('abrir')) {
+        ic.innerHTML = 'close';
+    } else {
+        ic.innerHTML = 'menu';
+    }
+}
